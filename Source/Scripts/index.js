1d3297e407c19f453cf4038bc736acba25111bb2
Mike <itsmike613@gmail.com>
2026-02-04 16:16:27 -0500

    More carrot drops

--- Source/Scripts/index.js
+++ Source/Scripts/index.js
@@ -108,7 +108,7 @@ const crops = {
     carrot: {
         seed: "seed_carrot",
         drop: { item: "carrot", min: 1, max: 2 },
-        bonus: { item: "seed_carrot", min: 1, max: 1 },
+        bonus: { item: "seed_carrot", min: 1, max: 2 },
         stages: [
             "./Source/Assets/Crops/Carrot/stage1.png",
             "./Source/Assets/Crops/Carrot/stage2.png",
