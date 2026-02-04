b57ab25fe2f866eba2796d62dd2eeaea6af29ddf
Mike <itsmike613@gmail.com>
2026-02-03 21:13:46 -0500

    Bonuses

--- Source/Scripts/index.js
+++ Source/Scripts/index.js
@@ -74,7 +74,7 @@ const crops = {
     wheat: {
         seed: "seed_wheat",
         drop: { item: "wheat", min: 1, max: 1 },
-        bonus: { item: "seed_wheat", min: 0, max: 2 },
+        bonus: { item: "seed_wheat", min: 1, max: 2 },
         stages: [
             "./Source/Assets/Crops/Wheat/stage1.png",
             "./Source/Assets/Crops/Wheat/stage2.png",
@@ -87,7 +87,7 @@ const crops = {
     carrot: {
         seed: "seed_carrot",
         drop: { item: "carrot", min: 1, max: 2 },
-        bonus: { item: "seed_carrot", min: 0, max: 1 },
+        bonus: { item: "seed_carrot", min: 1, max: 1 },
         stages: [
             "./Source/Assets/Crops/Carrot/stage1.png",
             "./Source/Assets/Crops/Carrot/stage2.png",
