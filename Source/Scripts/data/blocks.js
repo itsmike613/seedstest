aa8da0119c6184061d3a194f46236535c7a9696b
Mike <itsmike613@gmail.com>
2026-02-06 17:22:35 -0500

    Water cannot be broken by tool/hand, also new assets

--- Source/Scripts/data/blocks.js
+++ Source/Scripts/data/blocks.js
@@ -6,7 +6,7 @@ export const blocks = {
     unbreak: { k: "unbreak", img: "./Source/Assets/Blocks/unbreakable.png", breakable: false },
     tilled_dry: { k: "tilled_dry", img: "./Source/Assets/Blocks/unhydrated.png", breakable: true },
     tilled_wet: { k: "tilled_wet", img: "./Source/Assets/Blocks/hydrated.png", breakable: true },
-    water: { k: "water", img: "./Source/Assets/Blocks/water.png", breakable: true },
+    water: { k: "water", img: "./Source/Assets/Blocks/water.png", breakable: false },
     path: { k: "path", img: "./Source/Assets/Blocks/path.png", breakable: true },
     blueberry_bush_empty: { k: "blueberry_bush_empty", img: "./Source/Assets/Crops/Blueberry/empty_bush.png", breakable: true },
     blueberry_bush_full: { k: "blueberry_bush_full", img: "./Source/Assets/Crops/Blueberry/full_bush.png", breakable: true },
