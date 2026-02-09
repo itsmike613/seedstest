c299362d18266d63b271ccbe21c85f73fdcaa447
Mike <itsmike613@gmail.com>
2026-02-09 12:16:45 -0500

    Test smaller plot

--- Source/Scripts/config/constants.js
+++ Source/Scripts/config/constants.js
@@ -1,6 +1,6 @@
 // Source/Scripts/config/constants.js
 
-export const W = 32, H = 32;
+export const W = 10, H = 10;
 export const Y0 = 0, Y1 = 1;
 export const INF = { x: 0, y: 1, z: 0 };
 export const YB = Y1 + 1;
