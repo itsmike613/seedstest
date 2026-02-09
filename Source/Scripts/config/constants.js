a94ced25221438e6e522224153e347f975261db4
Mike <itsmike613@gmail.com>
2026-02-09 12:18:44 -0500

    Mini plot final

--- Source/Scripts/config/constants.js
+++ Source/Scripts/config/constants.js
@@ -1,6 +1,6 @@
 // Source/Scripts/config/constants.js
 
-export const W = 10, H = 10;
+export const W = 16, H = 16;
 export const Y0 = 0, Y1 = 1;
 export const INF = { x: 0, y: 1, z: 0 };
 export const YB = Y1 + 1;
